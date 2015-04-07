eebdda2d4b2c9a5ce823ee1cc3d733b12b6847ec
Louis Pilfold <louis@lpil.uk>
2015-04-07 23:54:45 +0100

    Update project metadata

--- mix.exs
+++ mix.exs
@@ -2,28 +2,25 @@ defmodule Palette.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :palette,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     deps: deps]
+    [
+      app: :palette,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      deps: deps,
+      name: "Palette",
+      source_url: "https://github.com/lpil/palette",
+      package: [
+        contributors: ["Louis Pilfold"],
+        licenses: ["MIT"],
+        links: %{ "GitHub" => "https://github.com/lpil/palette" }
+      ]
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:shouldi, only: :test}]
   end
