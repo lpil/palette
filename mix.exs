6e2c515eaf13e8afd5ead5dea7a3ff3c54c52210
Louis Pilfold <louis@lpil.uk>
2015-04-19 17:48:53 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Palette.Mixfile do
   def project do
     [
       app: :palette,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.0",
       deps: deps,
       name: "Palette",
